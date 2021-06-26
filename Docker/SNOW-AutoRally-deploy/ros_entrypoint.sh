10b6b6b777b42f54e127df9ddfc02347c7c140d5
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-06-26 18:49:29 -0400

    refactor dockerfiles (step 2)

--- Docker/SNOW-AutoRally-deploy/ros_entrypoint.sh
+++ Docker/SNOW-AutoRally-deploy/ros_entrypoint.sh
@@ -3,24 +3,24 @@
 # ref: https://github.com/dusty-nv/jetson-containers/tree/master/packages
 set -e
 
-ros_env_setup="/opt/ros/$ROS_DISTRO/setup.bash"
-echo "sourcing   $ros_env_setup"
-source "$ros_env_setup"
+ROS_ENV_SETUP="/opt/ros/$ROS_DISTRO/setup.bash"
+echo "sourcing   $ROS_ENV_SETUP"
+source "$ROS_ENV_SETUP"
 
 echo "ROS_ROOT   $ROS_ROOT"
 echo "ROS_DISTRO $ROS_DISTRO"
 
-ros_devel_env_setup=~/catkin_ws/devel/setup.bash
-echo "sourcing   $ros_devel_env_setup"
-source "$ros_devel_env_setup"
+ROS_DEVEL_ENV_SETUP=~/catkin_ws/devel/setup.bash
+echo "sourcing   $ROS_DEVEL_ENV_SETUP"
+source "$ROS_DEVEL_ENV_SETUP"
 
 echo "Make sure your workspace is properly overlayed by the setup script by checking the ROS_PACKAGE_PATH environment variable. "
 echo "It should include the directory you're in: /home/<youruser>/catkin_ws/src:/opt/ros/melodic/share"
 echo "ROS_PACKAGE_PATH $ROS_PACKAGE_PATH"
 
-joystickZero="/dev/input/js0"
+JOYSTICK_ZERO="/dev/input/js0"
 # The '-c' flag is a character device file test operator
-if [[ -c "$joystickZero" ]]; then
+if [[ -c "$JOYSTICK_ZERO" ]]; then
   chmod a+rw /dev/input/js0
 else
   echo "SNOW-AutoRally: No input device js0 detected"
@@ -33,6 +33,5 @@ fi
 #echo "AR_MPPI_PARAMS_PATH $AR_MPPI_PARAMS_PATH"
 #echo "AR_CONFIG_PATH $AR_CONFIG_PATH"
 #echo "AR_JOYSTICK $AR_JOYSTICK"
-
 
 exec "$@"
