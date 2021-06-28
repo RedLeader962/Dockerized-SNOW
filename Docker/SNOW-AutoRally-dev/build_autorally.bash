c9f295d5191913e99920d5e540e5d910c543e240
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-06-28 14:27:21 -0400

    feat dev run script

--- Docker/SNOW-AutoRally-dev/build_autorally.bash
+++ Docker/SNOW-AutoRally-dev/build_autorally.bash
@@ -22,12 +22,6 @@ source "/opt/ros/${ROS_DISTRO}/setup.bash"
 catkin_make
 source "${DEV_WORKSPACE}/devel/setup.bash"
 
-## AutoRally environment setup
-#autorally_env_setup="${DEV_WORKSPACE}/src/autorally/autorally_util/setupEnvLocal.sh"
-#echo "sourcing   $autorally_env_setup"
-#source "$autorally_env_setup"
-#
-#echo ". ${DEV_WORKSPACE}/src/autorally/autorally_util/setupEnvLocal.sh" >>~/.bashrc
 
 # AutoRally environment setup
 autorally_env_setup="${DEV_WORKSPACE}/src/autorally/autorally_util/setupEnvLocal.sh"
