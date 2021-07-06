27626c74f083f1e38e57c164ea3e0d4c4b42ed92
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-07-06 15:25:25 -0400

    NLSAR-170 | refactor readme and run_snow_deploy.bash script

--- Docker/snow-autorally-develop/ros_entrypoint.bash
+++ Docker/snow-autorally-develop/ros_entrypoint.bash
@@ -3,9 +3,9 @@
 #set -e  # exit script if any statement returns a non-true return value
 
 echo "
-      Use the SNOW-AutoRally build script to compile your code and source your environment
+Use the SNOW-AutoRally build script to compile your code and source your environment
 
-          root@snowxavier-desktop:/#  bash /build_snow_autorally.bash
-      "
+              root@snowxavier-desktop:/#  bash /build_snow_autorally.bash
+"
 
 exec "$@"
