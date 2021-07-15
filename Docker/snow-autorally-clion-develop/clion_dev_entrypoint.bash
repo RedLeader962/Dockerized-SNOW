cf22021fa1ca43be9107ae3d6be19ff90ba97f82
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-07-15 10:59:30 -0400

    NLSAR-177 | mod entrypoint

--- Docker/snow-autorally-clion-develop/clion_dev_entrypoint.bash
+++ Docker/snow-autorally-clion-develop/clion_dev_entrypoint.bash
@@ -10,18 +10,21 @@ echo "Starting container ssh server on port ${CLION_DEV_SERVER_PORT}"
 # -f : config_file
 #/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config_clion_snow_dev
 /usr/sbin/sshd -e -f /etc/ssh/sshd_config_clion_snow_dev
-echo "
-    To connect to the container:
 
-       $ ssh -p ${CLION_DEV_SERVER_PORT} ${NEW_USER}@$(hostname -I | awk '{print $1}')
+echo "
+    To connect remotely to the container:
+      Default user: ${CLION_DEV_USER}  pass: lasagne
+
+       $ ssh -p ${CLION_DEV_SERVER_PORT} ${CLION_DEV_USER}@$(hostname -I | awk '{print $1}')
        $ sftp -P ${CLION_DEV_SERVER_PORT} openssh-$(hostname -I | awk '{print $1}')
        $ scp -P ${CLION_DEV_SERVER_PORT} source target
-       $ scp -P ${CLION_DEV_SERVER_PORT} /path/to/foo ${NEW_USER}@$(hostname -I | awk '{print $1}'):/dest/
+       $ scp -P ${CLION_DEV_SERVER_PORT} /path/to/foo ${CLION_DEV_USER}@$(hostname -I | awk '{print $1}'):/dest/
 "
 echo "
 
-    Use the SNOW-AutoRally build script to compile your code and source your ROS environment:
-       bash /build_snow_autorally.bash
+    Use the SNOW-AutoRally build script to compile your code
+    and source your ROS environment:
+          \# bash /build_snow_autorally.bash
 
 "
 
