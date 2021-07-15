acb232a8e7b86ae509bb0e9a3a1c789ed9155f98
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-07-15 10:25:53 -0400

    NLSAR-177 | change container buisness logic regarding sshd

--- Docker/snow-autorally-clion-develop/clion_dev_entrypoint.bash
+++ Docker/snow-autorally-clion-develop/clion_dev_entrypoint.bash
@@ -3,14 +3,13 @@
 #set -e  # exit script if any statement returns a non-true return value
 
 echo
-echo "Start ssh server"
+echo "Starting container ssh server on port ${CLION_DEV_SERVER_PORT}"
 # sshd flag
 # -D : sshd will not detach and does not become a daemon. This allows easy monitoring of sshd.
 # -e : sshd will send the output to the standard error instead of the system log.
 # -f : config_file
 #/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config_clion_snow_dev
 /usr/sbin/sshd -e -f /etc/ssh/sshd_config_clion_snow_dev
-echo
 echo "
 
     Use the SNOW-AutoRally build script to compile your code and source your ROS environment:
