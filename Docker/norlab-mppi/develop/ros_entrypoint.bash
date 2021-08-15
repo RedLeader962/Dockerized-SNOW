9e3294177f927a34016507a8fb9992da9ae48d36
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-15 18:41:45 -0400

    NLSAR-214 | mod ros_entrypoint.bash comment

--- Docker/norlab-mppi/develop/ros_entrypoint.bash
+++ Docker/norlab-mppi/develop/ros_entrypoint.bash
@@ -7,7 +7,7 @@ echo "
     From inside the container, use the norlab-mppi build script to compile your development source code and source your ROS environment:
        root@norlab-og:/# bash /rebuild_norlab_mppi.bash
 
-    Note: Docker container user are by default sudo user. That's why it's `\#` instead of `\$`.
+    Note: Docker container user are by default sudo user. That's why it's # instead of $.
 "
 
 exec "$@"
