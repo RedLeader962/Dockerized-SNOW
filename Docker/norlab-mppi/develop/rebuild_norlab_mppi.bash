72540ed4176173408130298af8c0350c33689b3b
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-17 04:51:49 -0400

    NLSAR-230 | implement`norlab-mppi/ros-melodic-python3/Dockerfile`

--- Docker/norlab-mppi/develop/rebuild_norlab_mppi.bash
+++ Docker/norlab-mppi/develop/rebuild_norlab_mppi.bash
@@ -10,11 +10,18 @@ sudo apt-get update
 # rosdep install: looks at all the packages in the src directory and tries to find and install their dependencies on your platform
 rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro ${ROS_DISTRO} --default-yes
 
-# catkin_make_isolated step: rebuild everything in the catkin workspace DS_DEV_WORKSPACE
-python3 ./src/catkin/bin/catkin_make_isolated --install --install-space ${DS_DEV_WORKSPACE}/install_isolated -DCMAKE_BUILD_TYPE=Release \
-    &&  rm -rf /var/lib/apt/lists/*
+# catkin_make step: rebuild everything in the catkin workspace DS_DEV_WORKSPACE
+RUN /bin/bash -c "source /opt/ros/${ROS_DISTRO}/setup.bash \
+    && catkin_make \
+    && source ${DS_DEV_WORKSPACE}/devel/setup.bash" \
+    && echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc \
+    && echo "source ${DS_DEV_WORKSPACE}/devel/setup.bash" >> ~/.bashrc
 
-source "source ${DS_DEV_WORKSPACE}/install_isolated/setup.bash"
+## catkin_make_isolated step: rebuild everything in the catkin workspace DS_DEV_WORKSPACE
+#python3 ./src/catkin/bin/catkin_make_isolated --install --install-space ${DS_DEV_WORKSPACE}/install_isolated -DCMAKE_BUILD_TYPE=Release \
+#    &&  rm -rf /var/lib/apt/lists/*
+#
+#source "source ${DS_DEV_WORKSPACE}/install_isolated/setup.bash"
 
 
 # (Priority) todo:refactor (ref task NLSAR-222 🛠→ setupEnv*.sh scripts for deployement case)
