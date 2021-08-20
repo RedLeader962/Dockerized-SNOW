3f743f509958ebf499523996f2029b83c1dd62e7
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-20 12:55:42 -0400

    NLSAR-233 | Mod to sshserver config

--- Docker/norlab-mppi/develop/ros_entrypoint.bash
+++ Docker/norlab-mppi/develop/ros_entrypoint.bash
@@ -3,13 +3,13 @@
 #set -e  # exit script if any statement returns a non-true return value
 
 echo
-echo "Starting container ssh server on port ${DS_PYCHARM_DEV_SERVER_PORT}"
+echo "Starting container ssh server on port ${DS_PYCHARM_DEV_SERVER_PORT}\n"
 # sshd flag
 # -D : sshd will not detach and does not become a daemon. This allows easy monitoring of sshd.
 # -e : sshd will send the output to the standard error instead of the system log.
 # -f : config_file
-/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
-#/usr/sbin/sshd -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
+#/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
+/usr/sbin/sshd -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
 
 echo "
 To connect remotely to the container:
