c10ca580dffe5a16a150bb770685d380a78601f9
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-31 23:49:19 -0400

    NLSAR-255 | mod Powerline build step and config file

--- Docker/norlab-mppi/develop/ros_entrypoint.bash
+++ Docker/norlab-mppi/develop/ros_entrypoint.bash
@@ -3,8 +3,7 @@
 #set -e  # exit script if any statement returns a non-true return value
 
 echo
-echo -e "Starting container \033[1;37mssh server on port ${DS_PYCHARM_DEV_SERVER_PORT}\033[0m with \033[1;37muser ${DS_PYCHARM_DEV_USER}\033[0m (default pass: lasagne)
-"
+echo -e "Starting container \033[1;37mssh server on port ${DS_PYCHARM_DEV_SERVER_PORT}\033[0m with \033[1;37muser ${DS_PYCHARM_DEV_USER}\033[0m (default pass: lasagne)"
 # sshd flag
 # -D : sshd will not detach and does not become a daemon. This allows easy monitoring of sshd.
 # -e : sshd will send the output to the standard error instead of the system log.
@@ -12,15 +11,13 @@ echo -e "Starting container \033[1;37mssh server on port ${DS_PYCHARM_DEV_SERVER
 #/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
 /usr/sbin/sshd -e -f /etc/ssh/sshd_config_dockerized_snow_openssh_server
 
-echo -e "
-To connect remotely to the container:
+echo -e "To connect remotely to the container:
     $ ssh -p ${DS_PYCHARM_DEV_SERVER_PORT} ${DS_PYCHARM_DEV_USER}@$(hostname -I | awk '{print $1}')
     $ sftp -P ${DS_PYCHARM_DEV_SERVER_PORT} openssh-$(hostname -I | awk '{print $1}')
     $ scp -P ${DS_PYCHARM_DEV_SERVER_PORT} /path/to/foo ${DS_PYCHARM_DEV_USER}@$(hostname -I | awk '{print $1}'):/dest/
 "
 
-echo -e "
-Check if Python3 is working properly by running \033[1;37m\$ python3 /ros_catkin_ws/src/${DS_TARGET_PROJECT_SRC_REPO}/src/container_related/try_pytorch.py\033[0m in the container terminal.
+echo -e "Check if ${DS_PYTHON3_VERSION} is working properly by running \033[1;37m\$ python3 /ros_catkin_ws/src/${DS_TARGET_PROJECT_SRC_REPO}/src/container_related/try_pytorch.py\033[0m in the container terminal.
 If you need to rebuild your norlab-mppi ROS project, use \033[1;37m\$ bash /rebuild_norlab_mppi.bash\033[0m from inside the container,
 Recall that your project source code is mapped in the container at dir \033[1;37m/ros_catkin_ws/src/${DS_TARGET_PROJECT_SRC_REPO}/\033[0m
 "
