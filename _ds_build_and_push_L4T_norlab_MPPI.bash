d1dbaedc8e24f22fcc8ed241c8a16a10edc16674
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-09-06 14:23:29 -0400

    NLSAR-255 | implement build and push utility

--- _ds_build_and_push_L4T_norlab_MPPI.bash
+++ _ds_build_and_push_L4T_norlab_MPPI.bash
@@ -7,7 +7,8 @@
 #    echo "Stoping container $(docker stop ${CONTAINER_NAMES})"
 #fi
 
-if [ "$(uname -m)" == "aarch64" ]; then
+
+if [ uname -m == "aarch64" ]; then
 
   echo "Building norlab-mppi-dependencies:arm64-l4t-r32.6.1"
   sudo docker build -t norlabsnow/norlab-mppi-dependencies:arm64-l4t-r32.6.1 -f ./Docker/norlab-mppi/dependencies/Dockerfile  --build-arg BASE_IMAGE=norlabsnow/norlab-mppi-ros-melodic-python3:arm64-l4t-r32.6.1  ./Docker
@@ -17,3 +18,4 @@ if [ "$(uname -m)" == "aarch64" ]; then
   echo "norlab-mppi-dependencies:arm64-l4t-r32.6.1 builded and pushed to dockerhub"
 
 fi
+
