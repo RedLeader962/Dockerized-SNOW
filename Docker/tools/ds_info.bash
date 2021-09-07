87b910d4c81e6950a761e51c3e982c48a55ddf17
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-09-07 09:41:49 -0400

    NLSAR-255 | implemented ds_info.bash

--- Docker/tools/ds_info.bash
+++ Docker/tools/ds_info.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 PCK_VERSION=$(pip3 list --format freeze)
-
+SP="  "
 echo -e "
-\033[1;37mROS distro:         ${ROS_DISTRO}
-python3 version:    ${DS_PYTHON3_VERSION}\033[0m
-PyTorch version:    $(echo "${PCK_VERSION}" | grep torch | sed 's/torch==//g')
-PyCuda version:     $(echo "${PCK_VERSION}" | grep pycuda | sed 's/pycuda==//g')
-Numpy version:      $(echo "${PCK_VERSION}" | grep numpy | sed 's/numpy==//g')
-
-ROS package:        ${DS_ROS_PKG}
-ROS python version: ${ROS_PYTHON_VERSION}
-ROS master uri:     ${ROS_MASTER_URI}
-DS image aarch:     ${DS_IMAGE_ARCHITECTURE}
-DS src code path:   ${DS_DEV_WORKSPACE}${DS_TARGET_PROJECT_SRC_REPO}
+${DS_CONTAINER_NAME container info}\033[1;37m
+${SP}ROS distro:         ${ROS_DISTRO}
+${SP}python3 version:    ${DS_PYTHON3_VERSION}\033[0m
+${SP}PyTorch version:    $(echo "${PCK_VERSION}" | grep torch | sed 's/torch==//g')
+${SP}PyCuda version:     $(echo "${PCK_VERSION}" | grep pycuda | sed 's/pycuda==//g')
+${SP}Numpy version:      $(echo "${PCK_VERSION}" | grep numpy | sed 's/numpy==//g')
+${SP}ROS package:        ${DS_ROS_PKG}
+${SP}ROS python version: ${ROS_PYTHON_VERSION}
+${SP}ROS master uri:     ${ROS_MASTER_URI}
+${SP}DS image aarch:     ${DS_IMAGE_ARCHITECTURE}
+${SP}DS src code path:   ${DS_DEV_WORKSPACE}/${DS_TARGET_PROJECT_SRC_REPO}
 "
