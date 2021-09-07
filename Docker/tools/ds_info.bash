73fa472b81ca75c4b1cc1f1bf7c7f1becff6b724
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-09-07 09:46:12 -0400

    NLSAR-255 | fetch docker push status

--- Docker/tools/ds_info.bash
+++ Docker/tools/ds_info.bash
@@ -3,8 +3,8 @@
 PCK_VERSION=$(pip3 list --format freeze)
 SP="  "
 
-echo -e "
-${DS_CONTAINER_NAME} container info\033[1;37m
+#${DS_CONTAINER_NAME} container info
+echo -e "\033[1;37m
 ${SP}ROS distro:         ${ROS_DISTRO}
 ${SP}python3 version:    ${DS_PYTHON3_VERSION}\033[0m
 ${SP}PyTorch version:    $(echo "${PCK_VERSION}" | grep torch | sed 's/torch==//g')
