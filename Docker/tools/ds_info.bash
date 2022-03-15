21758435bc872c0a5bbf87ae25cd4ca32514d9d9
RedLeaderOne <luc.coupal.1@ulaval.ca>
2022-03-15 13:33:58 -0400

    NMO-128 | fix _ds_build_and_push_norlab_MPPI.bash instantiation step

--- Docker/tools/ds_info.bash
+++ Docker/tools/ds_info.bash
@@ -10,7 +10,7 @@ ${SP}python3 version:         ${DS_PYTHON3_VERSION}\033[0m
 ${SP}Numpy version:           $(echo "${PCK_VERSION}" | grep numpy | sed 's/numpy==//g')
 ${SP}PyTorch version:         $(echo "${PCK_VERSION}" | grep torch | sed 's/torch==//g')
 ${SP}Numba version:           $(echo "${PCK_VERSION}" | grep numba | sed 's/numba==//g')
-${SP}LLVMlite version:           $(echo "${PCK_VERSION}" | grep llvmlite | sed 's/llvmlite==//g')
+${SP}LLVMlite version:        $(echo "${PCK_VERSION}" | grep llvmlite | sed 's/llvmlite==//g')
 ${SP}ROS package:             ${DS_ROS_PKG}
 ${SP}ROS python version:      ${ROS_PYTHON_VERSION}
 ${SP}ROS master uri:          ${ROS_MASTER_URI}
